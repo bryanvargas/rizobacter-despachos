7f206bf1bc511548f142492c9e94a2f781652015
Bryan Vargas <bvargas@alamoconsultores.com>
2023-05-24 14:55:30 +0000

    dev001: proyecto inicial

---
+++ despacho/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ar/com/rizobacter/despacho/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ despacho/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"arcomrizobacter/despacho/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
